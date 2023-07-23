498921d1b26493b41f437ef242297a55b28d64a0
Ivan Evdokimychev <IAEvdokimychev@ya.ru>
2023-07-23 14:53:43 -0300

    finish Example003 in Seminar001

--- Seminar001/Example001/Program.cs
+++ Seminar001/Example001/Program.cs
@@ -2,7 +2,6 @@
 // a = 25, b = 5 -> да 
 // a = 2, b = 10 -> нет 
 // a = 9, b = -3 -> да 
-// a = -3, b = 9 -> нет
 
 // Организация ввода данных
 Console.Write("Введите целое число A: ");
@@ -15,14 +14,6 @@ int number2 = int.Parse(Console.ReadLine() ?? "");
 string result = "";
 if (number == number2 * number2) result = "да";
 else result = "нет";
-
-// Метод 1
-// Console.Write(number);
-// Console.Write(" -> ");
-// Console.WriteLine(result);
-
-// Метод 2 
-// Console.WriteLine(number + " -> " + result);
 
 // Метод 3 (интерполяция) - вывод данных
 Console.WriteLine($"a = {number}, b = {number2} -> {result}");
---
+++ Seminar001/Example003/Program.cs
@@ -0,0 +1,46 @@
+﻿// 3. Напишите программу, которая будет выдавать название дня недели по заданному номеру.
+// 3 -> Среда 
+// 5 -> Пятница
+
+// Организация ввода данных
+Console.Write("Введите номер дня недели: ");
+int numberDay = int.Parse(Console.ReadLine() ?? "");
+
+// Данные в программе из базы данных
+// string day1 = "Понедельник";
+// string day2 = "Вторник";
+// string day3 = "Среда";
+// string day4 = "Четверг";
+// string day5 = "Пятница";
+// string day6 = "Суббота";
+// string day7 = "Воскресение";
+
+// int numberDay1 = 1;
+// int numberDay2 = 2;
+// int numberDay3 = 3;
+// int numberDay4 = 4;
+// int numberDay5 = 5;
+// int numberDay6 = 6;
+// int numberDay7 = 7;
+
+// numberDay1 = day1;
+// numberDay2 = day2;
+// numberDay3 = day3;
+// numberDay4 = day4;
+// numberDay5 = day5;
+// numberDay6 = day6;
+// numberDay7 = day7;
+
+// Логика вычеслений
+string result = "такого дня недели нет";
+if (numberDay == 1) result = "Понедельник";
+if (numberDay == 2) result = "Вторник";
+if (numberDay == 3) result = "Среда";
+if (numberDay == 4) result = "Четверг";
+if (numberDay == 5) result = "Пятница";
+if (numberDay == 6) result = "Суббота";
+if (numberDay == 7) result = "Воскресение";
+
+// Метод 3 (интерполяция) - вывод данных
+Console.WriteLine($"День {numberDay} -> {result}");
+
