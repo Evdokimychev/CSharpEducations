2b12b5fc640b98aba5e51c19076e509444b33c0d
Ivan Evdokimychev <IAEvdokimychev@ya.ru>
2023-07-23 13:35:02 -0300

    add Example000 in Seminar001

---
+++ Seminar001/Example000/Program.cs
@@ -0,0 +1,19 @@
+﻿// Организация ввода данных
+Console.Write("Введите целое число: ");
+// string userInput = Console.ReadLine() ?? "";
+// int number = int.Parse(userInput);
+int number = int.Parse(Console.ReadLine() ?? "");
+
+// Логика вычеслений
+int result = number * number;
+
+// Метод 1
+// Console.Write(number);
+// Console.Write(" -> ");
+// Console.WriteLine(result);
+
+// Метод 2 
+// Console.WriteLine(number + " -> " + result);
+
+// Метод 3 (интерполяция) - вывод данных
+Console.WriteLine($"{number} -> {result}");
