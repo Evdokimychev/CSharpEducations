c2d17fabffe54f8b0077b33a325c744a062effee
Ivan Evdokimychev <IAEvdokimychev@ya.ru>
2023-07-24 14:50:55 -0300

    finish Example011_ArrayLibrary in Lection002

---
+++ Lection002/Example011_ArrayLibrary/Program.cs
@@ -0,0 +1,51 @@
+﻿void FillArray (int[] collection)
+{
+    int length = collection.Length;
+    int index = 0;
+    while (index < length)
+    {
+        collection[index] = new Random().Next(1, 10);
+        index++;
+    }
+}
+
+void PrintArray(int[] col)
+{
+    int count = col.Length;
+    int position = 0;
+    while (position < count)
+    {
+        Console.WriteLine (col[position]);
+        position++;
+        // return использовать в функции void не нужно
+    }
+}
+
+int IndexOf (int[] collection, int find)
+{
+    int count = collection.Length;
+    int index = 0;
+    int position = -1; // Чтобы обозначить, что элемент не найден
+    while (index < count)
+    {
+        if (collection[index] == find)
+        {
+            position = index;
+            break;
+        }
+        index++;
+    }
+    return position;
+}
+
+int[] array = new int[10]; // Создание нового массива с 10 элементами
+
+FillArray(array);
+array[4] = 4;
+array[6] = 4;
+
+PrintArray(array);
+Console.WriteLine();
+
+int pos = IndexOf(array, 444);
+Console.WriteLine(pos);
