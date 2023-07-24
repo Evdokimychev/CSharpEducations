66d5cc5441112f26b1f72aac40c01f22006eb15d
Ivan Evdokimychev <IAEvdokimychev@ya.ru>
2023-07-24 14:22:34 -0300

    finish Example010_MethodArray in Lection002

---
+++ Lection002/Example010_MethodArray/Program.cs
@@ -0,0 +1,22 @@
+﻿int[] array = {1, 12, 31, 4, 18, 15, 16, 17, 18};
+
+int n = array.Length; // Определяем количество элементов в массиве
+
+// Организация ввода данных
+Console.Write("Введите целое число: ");
+int find = int.Parse(Console.ReadLine() ?? "");
+
+// Логика решения
+int index = 0;
+
+while (index < n)
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine ($"Это элемент под номером: {index}");
+        break;
+    }
+    index++; // index = index + 1
+}
+
+// Console.WriteLine ($"Этот элемент не найден");
