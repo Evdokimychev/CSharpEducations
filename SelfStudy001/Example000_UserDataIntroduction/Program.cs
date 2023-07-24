3952de5d9ea574ce8bcb590d818489b3c190fed4
Ivan Evdokimychev <IAEvdokimychev@ya.ru>
2023-07-24 15:06:41 -0300

    finish Example000_UserDataIntroduction in SelfStudy001

---
+++ SelfStudy001/Example000_UserDataIntroduction/Program.cs
@@ -0,0 +1,19 @@
+﻿int Max (int arg1, int arg2, int arg3)
+{
+    int result = arg1;
+    if (arg2 > result) result = arg2;
+    if (arg3 > result) result = arg3;
+    return result;
+}
+
+int GetData()
+{
+    Write("Введите значение: ");
+    return ToInt32 (ReadLine());
+}
+
+int max = Max (
+                Max(GetData(), GetData(), GetData()),
+                Max(GetData(), GetData(), GetData()),
+                Max(GetData(), GetData(), GetData())
+                );
