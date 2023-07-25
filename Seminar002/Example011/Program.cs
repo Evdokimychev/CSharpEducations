9b568142b09462e08391b9d8a72404a2389139d7
Ivan Evdokimychev <IAEvdokimychev@ya.ru>
2023-07-25 17:11:13 -0300

    finish Example009 in Seminar002

--- Seminar002/Example011/Program.cs
+++ Seminar002/Example011/Program.cs
@@ -9,6 +9,7 @@ Console.Clear();
 
 int num = new Random().Next(100, 1000);
 int a1 = num / 100;
+int a2 = num / 10 % 10;
 int a3 = num % 10;
 
 // Console.WriteLine($"Новое число полученное из {num}: -> {a1}{a3}");
