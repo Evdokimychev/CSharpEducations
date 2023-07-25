bb9d52c26b3f6e36a1d94756d2d4a4e5187c0fd2
Ivan Evdokimychev <IAEvdokimychev@ya.ru>
2023-07-25 17:15:07 -0300

    finish Example009 in Seminar002

--- Seminar002/Example011/Program.cs
+++ Seminar002/Example011/Program.cs
@@ -8,12 +8,11 @@
 Console.Clear();
 
 int num = new Random().Next(100, 1000);
+
 int a1 = num / 100;
 int a2 = num / 10 % 10;
 int a3 = num % 10;
-
-// Console.WriteLine($"Новое число полученное из {num}: -> {a1}{a3}");
-
 int result = a1 * 10 + a3;
 
+// Console.WriteLine($"Новое число полученное из {num}: -> {a1}{a3}");
 Console.WriteLine($"Новое число полученное из {num}: -> {result}");
