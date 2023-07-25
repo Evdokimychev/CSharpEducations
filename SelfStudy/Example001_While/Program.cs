06d66883449b19452e3e596d197f733da9bb254d
Ivan Evdokimychev <IAEvdokimychev@ya.ru>
2023-07-25 15:53:49 -0300

    finish Example001_While in SelfStudy and finish Example009 in Seminar002

---
+++ SelfStudy/Example001_While/Program.cs
@@ -0,0 +1,14 @@
+﻿// Циклический ввод переменной типа double
+// Вводим переменную
+double varA;
+
+// // Организовываем цикл после неправильного воода
+while (true)
+{
+        Console.Write ("Введите А: ");
+        if (double.TryParse (Console.ReadLine(), out varA))
+              break;
+        Console.WriteLine ("Ошибка ввода!");
+}
+
+Console.WriteLine($"A = {varA}");
