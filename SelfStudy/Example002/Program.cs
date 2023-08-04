18bbd907b9bef000e5f4d3ea02599b5d29e8a114
Ivan Evdokimychev <IAEvdokimychev@ya.ru>
2023-08-04 17:04:36 -0300

    finish Example002 in SelfStudy

---
+++ SelfStudy/Example002/Program.cs
@@ -0,0 +1,18 @@
+﻿// double[] a = new double[5];
+// Random random = new Random();
+// for (int i = 0; i < 5; i++)
+// {
+//     a[i] = random.NextDouble() * 100; // NextDouble() дает случайное вещественное число в диапазоне от 0 до 1
+//         Console.Write("{0,6:F2}", a[i]);
+
+//     Console.WriteLine();
+// }
+ double[] res = new double[5];
+    for (int i = 0; i < 5; i++)
+    {
+        res[i] = new Random().NextDouble() * 100;
+            Console.Write("{0,6:F2}", res[i]);
+        
+        Console.WriteLine();
+
+    }
